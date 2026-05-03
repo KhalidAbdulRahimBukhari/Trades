7ab860d2637a2ad2a5789f8da9be3e4aa063aa6b
khalid <k.bukhari.1418@gmail.com>
2026-05-03 15:47:01 +0200

    Backend: Main function

---
+++ backend/src/main/java/com/trade/system/TradeProcessingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.trade.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeProcessingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeProcessingSystemApplication.class, args);
+    }
+}
